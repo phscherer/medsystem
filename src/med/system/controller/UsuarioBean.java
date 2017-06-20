b08d64d28691cd124324d6ba096152a0fdfe5465
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-06-20 13:22:21 -0300

    Ajusta paginação e messages

--- src/med/system/controller/UsuarioBean.java
+++ src/med/system/controller/UsuarioBean.java
@@ -1,9 +1,12 @@
 package med.system.controller;
 
+import java.io.IOException;
 import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+
 import med.system.dao.UsuarioDAO;
 import med.system.entity.Usuario;
 
@@ -14,13 +17,17 @@ public class UsuarioBean {
 	private UsuarioDAO usuarioDao = new UsuarioDAO();
 	
 	
-	public String salva() {
-		this.usuarioDao.salva(this.usuario);
-		this.usuario = new Usuario();
-		FacesMessage msg = new FacesMessage("Paciente cadastrado com sucesso!");
-		FacesContext.getCurrentInstance().addMessage(null, msg);
-		
-		return "index.xhtml";
+	public void salva() throws IOException {
+		if (this.usuario.getNomeUsuario().trim().isEmpty() || this.usuario.getSenha().trim().isEmpty()) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERRO!", "Informe os dados corretamente."));
+		} else {
+			this.usuarioDao.salva(this.usuario);
+			this.usuario = new Usuario();
+			
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "SUCESSO!", "Paciente cadastrado com sucesso!"));
+			FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+		}
 	}
 	
 	public void exclui(Usuario usuario) {
